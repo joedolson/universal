32d46ff011ac6afdb8b9f4f3b05d501377d9986e
Joe Dolson <design@joedolson.com>
2021-02-01 12:36:58 -0600

    Trigger actions

--- page.php
+++ page.php
@@ -1,11 +1,13 @@
 <?php get_header(); ?>
 
-    <?php if (have_posts()) : ?>
-    <?php while (have_posts()) : the_post(); ?>
+	<?php
+	if (have_posts()) :
+		while (have_posts()) : the_post();
+	?>
 		<section>
 		<?php if ( has_post_thumbnail() ) { ?>
 			<div class='featured-image'><?php the_post_thumbnail(); ?></div>
-		<?php } ?>		
+		<?php } ?>
 		<h1 class="page-title" id="title-<?php the_ID(); ?>"><?php the_title(); ?></h1>
 
 		<div <?php post_class( 'post-content' ); ?> id="post-<?php the_ID(); ?>">
@@ -18,20 +20,16 @@
 		<?php trackback_rdf(); ?>
 		-->
 		
-    <?php endwhile; ?>
-
-	<?php 
+	<?php
+		endwhile; 
 		if ( comments_open() ) {
 			comments_template(); 
 		}
-	?>
+	else :
 
+		get_template_part( 'no-posts' );
+	endif;
 
-	<?php else :
-	
-		get_template_part( 'no-posts' );	
-	
-	endif; ?>
-
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+	get_sidebar();
+	get_footer();
+?>
